091d8cd4eddd86f55d158af066a60cece6e32176
sen <33885101+anonymousculprit@users.noreply.github.com>
2021-04-15 17:18:30 +0800

    update #10
    oh god look at these art assets

---
+++ Assets/Scripts/Animation/BoneMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneMove : MonoBehaviour
+{
+    public GameObject[] target;
+    public float strength, timeMultiplier;
+    float r = 0;
+
+    void Update()
+    {
+        for (int i = 0; i < target.Length; i++)
+        {
+            r += Time.deltaTime * timeMultiplier;
+            float x = Mathf.Cos(r) * 0.001f * strength;
+
+            target[i].transform.position += new Vector3(x, 0, 0);
+        }
+    }
+
+    void MoveVersion1()
+    {
+        float rotation = 0f;
+
+        rotation += Time.deltaTime;
+
+        Transform b1 = transform.GetChild(0);
+        float rotationLim = 5f;
+
+        do
+        {
+            b1.rotation = Quaternion.Euler(0, 0, 90 + rotationLim * Mathf.Cos(rotation));
+            rotationLim += rotationLim * 0.5f;
+
+            if (b1.childCount == 0) break;
+            b1 = b1.GetChild(0);
+        } while (true);
+    }
+}
